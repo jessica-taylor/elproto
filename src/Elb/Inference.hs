ddd715ac6d9ab3b26dd239eb2f15892680282a4f
Jacob Taylor <jacobltaylor@gmail.com>
2012-08-26 13:03:42 -0700

    improved inference

--- src/Elb/Inference.hs
+++ src/Elb/Inference.hs
@@ -1,14 +1,19 @@
 module Elb.Inference where
 
+import Elb.InvFun (InvFun, sampler)
+import Elb.LogProb (LogProb)
+import Elb.Sampler (runSamplerRand)
+import Elb.Utils (undoI)
+
 scoredHypothesis :: (RandomGen g) => InvFun () a -> (a -> InvFun () b) 
                  -> (b -> InvFun () a) -> b -> Rand (a, LogProb)
 scoredHypothesis prior obsFun posterior obs = do
   -- P(H|O)
-  (hyp, posteriorProb) <- sample (posterior obs) ()
+  (hyp, posteriorProb) <- runSamplerRand $ sample (posterior obs) ()
   -- 1 / P(O|H)
-  ((), invObsProb) <- sample (undoI (obsFun hyp)) obs
+  ((), invObsProb) <- runSamplerRand $ sample (undoI (obsFun hyp)) obs
   -- 1 / P(H)
-  ((), invPriorProb) <- sample (undoI prior) hyp
+  ((), invPriorProb) <- runSamplerRand $ sample (undoI prior) hyp
   -- P(O) = P(H) P(O|H) / P(H|O)
   -- better P(O) means better posterior
   return (hyp, 1 / (invPriorProb * invObsProb * posteriorProb))
@@ -17,19 +22,21 @@ scoredPosterior :: (RandomGen g) => InvFun () a -> (a -> InvFun () b)
                 -> (b -> InvFun () a) -> Rand LogProb
 scoredPosterior prior obsFun posterior = do
   -- P(F)
-  (fact, factProb) <- sample prior ()
+  (fact, factProb) <- runSamplerRand $ sample prior ()
   -- P(O|F)
-  (obs, obsFactProb) <- sample (obsFun fact) ()
+  (obs, obsFactProb) <- runSamplerRand $ sample (obsFun fact) ()
   -- 1 / P(F|O)
-  ((), invPosteriorFactProb) <- sample (undoI (posterior obs)) fact
+  ((), invPosteriorFactProb) <- runSamplerRand $ 
+                                  sample (undoI (posterior obs)) fact
   -- P(H|O)
-  (hyp, posteriorHypProb) <- sample (posterior obs) ()
+  (hyp, posteriorHypProb) <- runSamplerRand $ sample (posterior obs) ()
   -- 1 / P(O|H)
-  ((), invObsHypProb) <- sample (undoI (obsFun hyp)) obs
+  ((), invObsHypProb) <- runSamplerRand $ sample (undoI (obsFun hyp)) obs
   -- 1 / P(H)
-  ((), invHypProb) <- sample (undoI prior) hyp
+  ((), invHypProb) <- runSamplerRand $ sample (undoI prior) hyp
   -- P(H) P(O|H) P(F|O) / (P(F) P(O|F) P(H|O))
-  -- = P(H,F,O) / P(H,F,O)
+  -- = (P(H) P(O|H) / P(H|O)) / (P(F) P(O|F) / P(F|O))
+  -- = P(O) / P(O)
   -- Yeah I'm not sure what this means but it seems like it should work.
   return (1 / (factProb * obsFactProb * invPosteriorFactProb *
                posteriorHypProb * invObsHypProb * invHypProb))
