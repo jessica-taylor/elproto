a1a04de6635c4c12cf3977ed0c2063e5b6aef0f4
Mario Villaplana <mario.villaplana@gmail.com>
2012-08-26 12:48:27 -0700

    Documented LogProb.hs

--- src/Elb/LogProb.hs
+++ src/Elb/LogProb.hs
@@ -1,19 +1,29 @@
+-- |'Elb.LogProb' exports the basic type for log probability and several
+-- functions for basic operations on log probability.
 module Elb.LogProb (
   LogProb, toLogProb, fromLogProb, 
   logToLogProb, logFromLogProb
 ) where
 
+-- |'LogProb' is a type for log probabilities.
 newtype LogProb = LogProb { logFromLogProb :: Double } deriving (Eq, Ord, Show)
 
+-- |'logToLogProb' casts a Double representing a log probability as a LogProb.
 logToLogProb :: Double -> LogProb
 logToLogProb = LogProb
 
+-- |'toLogProb' takes a Double representing a probability and returns the log
+-- probability.
 toLogProb :: Double -> LogProb
 toLogProb = logToLogProb . log
 
+-- |'fromLogProb' takes a LogProb and returns the non-logarithmic probability
+-- as a Double.
 fromLogProb :: LogProb -> Double
 fromLogProb = exp . logFromLogProb
 
+-- |'Num LogProb' defines basic arithmetic operations (addition, subtraction,
+-- multiplication, and division) for working with log probabilities.
 instance Num LogProb where
   LogProb a + LogProb b 
     | a >= b = LogProb (a + log (1 + exp (b - a)))
@@ -23,5 +33,8 @@ instance Num LogProb where
     | otherwise = error "Result of LogProb subtraction is negative"
   LogProb a * LogProb b = LogProb (a + b)
   LogProb a / LogProb b = LogProb (a - b)
-
-    
+  -- TODO(mario) Put in more fns from Num. Make sure this works as users would
+  -- expect it to work.
+  -- TODO(mario) Minimize runtime errors since people may expect behavior of
+  -- Num.
+  -- TODO(mario) Try to use this for basic examples (LogReg, Naive Bayes). 
