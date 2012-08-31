fa98a6847e58b8ad2ad0a77848eff124d130a6a7
Mario Villaplana <mario.villaplana@gmail.com>
2012-08-30 21:47:34 -0700

    Remove Mario's annoying comments. Now in annoying branch.

--- src/Elb/PureInvFun.hs
+++ src/Elb/PureInvFun.hs
@@ -9,17 +9,13 @@ import Control.Monad.Error ()
 data PureInvFun a b = 
   PureInvFun (a -> Either String b) (b -> Either String a)
     (a -> b -> a -> String) (b -> a -> b -> String)
-  -- mario should learn how these error messages work
-  -- arguments: g(f(x)) -> f(x) -> x -> error! if x is not really x
+
 errorless :: (a -> b) -> (b -> a) -> PureInvFun a b
 errorless f invF = PureInvFun (return . f) (return . invF) undefined undefined
 
 call :: Eq a => PureInvFun a b -> a -> Either String b
 call (PureInvFun f invF err _) x = do
-  -- f x is an Either String b
-  -- res is just a b
   res <- f x
-  -- x' == f^-1(x)
   x' <- invF res
   if x == x' then return res else fail (err x res x')
 
