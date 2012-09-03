b7a8c98b897cfb402db9f4cb6c8fe92fdea7e855
Mario Villaplana <mario.villaplana@gmail.com>
2012-09-02 20:30:14 -0700

    Document some of Utils.hs. Moving on to study mode! woot

--- src/Elb/Utils.hs
+++ src/Elb/Utils.hs
@@ -8,21 +8,30 @@ import Elb.InvFun
 import Elb.PureInvFun
 import Elb.Syntax
 
+-- |'PureI' transforms a pure invertible function into an invertible function.
+-- TODO(mario/jacob) Mario may need help isolating the exact difference between
+-- PureInvFun and InvFun, although he'll read more source to try to understand.
 pureI :: (Eq a, Eq b) => PureInvFun a b -> InvFun a b
 pureI = Pure
 
+-- |'composeI' makes invertible functions transitive by composing them --
+-- pretty simple.
 composeI :: (Eq a, Eq b, Eq c) => InvFun a b -> InvFun b c -> InvFun a c
 composeI = Compose
 
 subcallI :: (Eq a, Eq b, Eq c) => (a -> InvFun b c) -> InvFun (a, b) (a, c)
 subcallI = Subcall
 
+-- 'undoI' reverses an invertible function.
 undoI :: (Eq a, Eq b) => InvFun a b -> InvFun b a
 undoI = Undo
 
+-- 'flipI' emulates flipping a coin, taking a 'Double' representing the
+-- probability of True or False.
 flipI :: Double -> InvFun () Bool
 flipI = Flip
 
+-- | 'idI' gives the identity function.
 idI :: Eq a => InvFun a a
 idI = Pure (errorless id id)
 
@@ -30,6 +39,8 @@ idI = Pure (errorless id id)
 returnI :: Eq a => a -> InvFun () a
 returnI x = Pure (errorless (const x) (const ()))
 
+-- | 'appliedI' applies an invertible function given the first parameter and
+-- returns an invertible function from nothing () to the result.
 appliedI :: (Eq a, Eq b) => InvFun a b -> a -> InvFun () b
 appliedI f x = composeI (returnI x) f
 
